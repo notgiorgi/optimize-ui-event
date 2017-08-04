c0683759719266694b06fe12c32dbe908a3073b1
Giorgi Bagdavadze <notgiorgi@gmail.com>
2017-08-04 16:53:18 +0400

    linted

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-module.exports = function optimizeEvent(type, obj = window) {
-    let running = false
+/* eslint-env browser */
+module.exports = function optimizeEvent (type, obj = window) {
+  let running = false
 
-    obj.addEventListener(type, () => {
-        if (running) return
-        running = true
-        requestAnimationFrame(() => {
-            obj.dispatchEvent(new CustomEvent(`optimized-${type}`))
-            running = false
-        })
+  obj.addEventListener(type, () => {
+    if (running) return
+    running = true
+    requestAnimationFrame(() => {
+      obj.dispatchEvent(new CustomEvent(`optimized-${type}`))
+      running = false
     })
+  })
 }
