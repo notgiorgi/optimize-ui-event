86ac1b493e88f295af6e5a0ad58d5fd769733304
Giorgi Bagdavadze <notgiorgi@gmail.com>
2017-08-04 16:47:51 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports =  function optimizeEvent(type, obj = window) {
+    let running = false
+
+    obj.addEventListener(type, () => {
+        if (running) { return; }
+        running = true;
+        requestAnimationFrame(() => {
+            obj.dispatchEvent(new CustomEvent(`optimized-${type}`))
+            running = false;
+        })
+    })
+}
